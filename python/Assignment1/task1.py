0414c47112ea5ffd8ce80b1be665a41972c7d8c8
Vaibhav Haldia <vaibhavgsh@gmail.com>
2025-08-09 16:45:49 +0530

    Create task1 and task2 done

---
+++ python/Assignment1/task1.py
@@ -0,0 +1,14 @@
+#task 1 : Perform Basic Mathematical Operations
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+print("Addition: ", num1+num2)
+print("Subtraction: ", num1-num2)
+print("Multiplication: ", num1*num2)
+print("Division: ", num1/num2)
+
+# task 2: Create a Personalized Greeting
+name1 = input("Enter the first name: ")
+name2 = input("Enter the second name: ")
+full_name = name1 + " " + name2
+print(f"Hello, {full_name}! Welcome to the Python program.")
